56b9f3a2582d0eca289533d4d42983e3e215ef9f
Dids Irwyn Reyes <dids.irwyn@gmail.com>
2023-11-18 17:51:38 +0800

    Update Memories

---
+++ client/src/api/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const url = "http://localhost:9000/posts";
+
+export const fetchPost = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
